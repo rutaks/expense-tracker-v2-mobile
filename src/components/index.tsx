9818cf2957bb697a8776a7aee3d925a3e8ffd925
rutaks <samrutakayile@awesomity.rw>
2020-12-28 05:49:32 +0200

    Add Financial record listing component
    - Add Financial record item styling
    - Setup financial record listing styling

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,3 +3,5 @@ export {default as Button} from './Button';
 export {default as HomeTopBar} from './HomeTopBar';
 export {default as IconButton} from './IconButton';
 export {default as CurrentBalanceArea} from './CurrentBalanceArea';
+export {default as TransactionList} from './TransactionList';
+export {default as TransactionListItem} from './TransactionListItem';
