b741fcd275e981cb6e74652c409d9da8cefcd9da
rutaks <samrutakayile@awesomity.rw>
2021-02-01 09:31:51 +0200

    Pass actual date

--- src/components/DateField/DateField.tsx
+++ src/components/DateField/DateField.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {Text, TouchableOpacity} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {Colors} from '../../styles';
+import {toCustomDateFormat} from '../../utils/formatting.util';
 import {styles} from './DateField.styles';
 
-const DateField = (props: {onDateSelected?: () => {}}) => {
+const DateField = (props: {date: Date; onPressOut?: () => any}) => {
   console.log(props);
   return (
-    <TouchableOpacity style={styles.row}>
-      <Text style={styles.dateText}>Tuesday, 25 September</Text>
+    <TouchableOpacity onPressOut={props.onPressOut} style={styles.row}>
+      <Text style={styles.dateText}>
+        {toCustomDateFormat(props?.date || new Date())}
+      </Text>
       <MaterialCommunityIcons
         size={28}
         color={Colors.LIGHTER_BLUE}
