21e5e3e7e477af3bc2b2a6597fdde3336c000a6b
rutaks <samrutakayile@awesomity.rw>
2021-02-03 07:24:08 +0200

    Integrate getting expense & income on top info view
    - Add utils to calculate current balance, total expense & income
    - Modified spacing on top and bottom of component

--- src/components/CurrentBalanceArea/CurrentBalanceArea.tsx
+++ src/components/CurrentBalanceArea/CurrentBalanceArea.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import FinancialRecord from '../../models/financial-record.model';
-import {Dimensions} from '../../styles';
-import {getTotalAmount} from '../../utils/financial-record.util';
+import {Colors, Dimensions} from '../../styles';
+import {
+  getExpenseSum,
+  getIncomeSum,
+  getTotalAmount,
+} from '../../utils/financial-record.util';
 import {numberWithCommas} from '../../utils/formatting.util';
-// import {SubBalanceArea} from './components';
+import {SubBalanceArea} from './components';
 import {styles} from './CurrentBalanceArea.styles';
 
 /**
@@ -25,15 +29,29 @@ const CurrentBalanceArea = (props: {items: FinancialRecord[]}) => {
       ]}>
       <View style={styles.currentBalanceContainer}>
         <Text style={styles.currentBalanceHeading}>Current Balance</Text>
+        <Text
+          style={{
+            ...styles.currentBalanceHeading,
+            color: Colors.BLACK,
+            paddingTop: Dimensions.SIZE_SM,
+          }}>
+          RWF
+        </Text>
         <Text style={styles.currentBalanceAmout}>
-          RWF {numberWithCommas(getTotalAmount(props?.items).toString())}
+          {numberWithCommas(getTotalAmount(props?.items)?.toString())}
         </Text>
       </View>
-      {/* <View style={styles.subBalanceArea}>
-        <SubBalanceArea title="Income" amount={'4,500'} />
+      <View style={styles.subBalanceArea}>
+        <SubBalanceArea
+          title="Income"
+          amount={numberWithCommas(getIncomeSum(props?.items)?.toString())}
+        />
         <View style={{paddingTop: Dimensions.SIZE_M}} />
-        <SubBalanceArea title="Expense" amount={'4,500'} />
-      </View> */}
+        <SubBalanceArea
+          title="Expense"
+          amount={numberWithCommas(getExpenseSum(props?.items)?.toString())}
+        />
+      </View>
     </View>
   );
 };
