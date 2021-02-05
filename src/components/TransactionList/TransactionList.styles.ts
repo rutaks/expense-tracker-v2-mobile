f79b077632f0d14442ae9c2ce10256e7527b4aca
rutaks <samrutakayile@awesomity.rw>
2021-02-05 10:03:31 +0200

    Fix flat list scroll issue

---
+++ src/components/TransactionList/TransactionList.styles.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import {Dimensions} from '../../styles';
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingLeft: Dimensions.SIZE_L,
+    paddingRight: Dimensions.SIZE_L,
+    flex: 1,
+  },
+});
